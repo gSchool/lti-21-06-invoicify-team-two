baa1e887dcb7ffcb2e7957d62298dd4e35e3b7b0
unknown <Lntinfotech@INFEDL31060.lntinfotech.com>
2021-07-08 15:03:12 -0500

    Adding the user Endpoint controller class

---
+++ src/main/java/com/example/invoicify/controllers/UserController.java
@@ -0,0 +1,16 @@
+package com.example.invoicify.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @GetMapping("/users")
+    public String getUsers() {
+        return "Invoicify Users";
+    }
+
+}
