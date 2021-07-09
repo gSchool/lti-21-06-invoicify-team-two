17eb45f910fde1fa515147ab3a4b4b317a0876f5
jeanmarcjulien <julienjeanmarc@gmail.com>
2021-07-08 20:36:53 -0500

    adds H2 connection in application.properties file

--- src/main/java/com/galvanize/invoicify/InvoicifyApplication.java
+++ src/main/java/com/galvanize/invoicify/InvoicifyApplication.java
@@ -1,13 +1,14 @@
-package com.example.invoicify;
+package com.galvanize.invoicify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"com.galvanize"})
 public class InvoicifyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InvoicifyApplication.class, args);
 	}
-
 }
