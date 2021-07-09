833ecdebeb3d6ab87e769777680d49be30a05516
Naushaba Tabassum <tnaushaba@yahoo.com>
2021-07-09 11:18:37 -0500

    Update model

---
+++ src/main/java/com/galvanize/invoicify/models/Company.java
@@ -0,0 +1,58 @@
+package com.galvanize.invoicify.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO )
+    private Long id;
+
+
+    @Column(length = 255)
+    private String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "company")
+    private String company;
+
+    public Company() {
+    }
+
+    public Company(String name, String company) {
+        this.name = name;
+        this.company = company;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", company='" + company + '\'' +
+                '}';
+    }
+}
