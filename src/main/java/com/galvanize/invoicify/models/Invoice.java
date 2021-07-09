7cd9f2da1d7a5ac473efc242955e32fcee49c220
Elias Binchamo <eliasbinchamo@gmail.com>
2021-07-09 15:37:55 -0400

    Added get login session test -test failed

--- src/main/java/com/galvanize/invoicify/models/Invoice.java
+++ src/main/java/com/galvanize/invoicify/models/Invoice.java
@@ -2,7 +2,7 @@ package com.galvanize.invoicify.models;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+//import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.*;
 import java.sql.Date;
--- src/test/java/com/example/invoicify/UserControllerTest.java
+++ src/test/java/com/example/invoicify/UserControllerTest.java
@@ -1,21 +1,47 @@
 package com.example.invoicify;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.galvanize.invoicify.models.User;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+//
+//import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+
+//@SpringBootTest
+//@AutoConfigureMockMvc
+@WebMvcTest
 public class UserControllerTest {
+
+    User user = new User();
 
     @Autowired
     MockMvc mvc;
@@ -27,4 +53,27 @@ public class UserControllerTest {
                 .andExpect(content().string("Invoicify Users"));
     }
 
+//    As a user want to login,
+//    given I have a valid ID and password
+//
+//    POST http://localhost:8080/api/session
+
+    @Test
+    public void getUserLoginSession() throws Exception {
+        ObjectMapper userMap = new ObjectMapper();
+        user.setId(1L);
+        user.setUsername("bob");
+        user.setPassword("password");
+
+        String userObject = userMap.writeValueAsString(user);
+        System.out.println(userObject);
+        RequestBuilder request = post("/api/session")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userObject);
+
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username", equalTo("bob" )))
+                .andExpect(jsonPath("$.password", equalTo("password" )));
+    }
 }
