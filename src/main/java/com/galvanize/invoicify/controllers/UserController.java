5dac881fcc02f79b4c7568e41e13012732f5a296
jeanmarcjulien <julienjeanmarc@gmail.com>
2021-07-08 20:31:52 -0500

    adds controllers

--- src/main/java/com/galvanize/invoicify/controllers/UserController.java
+++ src/main/java/com/galvanize/invoicify/controllers/UserController.java
@@ -1,16 +1,49 @@
-package com.example.invoicify.controllers;
+package com.galvanize.invoicify.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.galvanize.invoicify.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.galvanize.invoicify.models.User;
+
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/user")
 public class UserController {
 
-    @GetMapping("/users")
-    public String getUsers() {
-        return "Invoicify Users";
-    }
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private PasswordEncoder encoder;
+
+	@PutMapping("{id}")
+	public User updateUser(Authentication auth, @RequestBody User user, @PathVariable Long id) {
+		User currentUserData = this.userRepository.findOne(id);
+		user.setId(currentUserData.getId());
+
+		if (user.getPassword() == null) {
+			user.setPassword(currentUserData.getPassword());
+		} else {
+			String encryptedPassword = encoder.encode(user.getPassword());
+			user.setPassword(encryptedPassword);
+		}
+
+		return userRepository.save(user);
+	}
+
+	@PostMapping
+	public User createUser(@RequestBody User user) {
+		String password = user.getPassword();
+		String encryptedPassword = encoder.encode(password);
+		user.setPassword(encryptedPassword);
+		userRepository.save(user);
+		return user;
+	}
 
 }
