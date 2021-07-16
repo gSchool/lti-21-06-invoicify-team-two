5f7907d837682cb1843dfbf3ffc5739bf1b555d0
Romanov Andre <romanovandre05@gmail.com>
2021-07-16 10:21:42 -0400

    User stories in progress...

---
+++ src/main/java/com/galvanize/invoicify/controllers/InvoiceController.java
@@ -0,0 +1,10 @@
+package com.galvanize.invoicify.controllers;
+
+import com.galvanize.invoicify.models.InvoiceLineItem;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+public class InvoiceController {
+
+}
