ade6aeff19637183899edf266ade1494ed622db8
Lisa Lampen <monalisa408@yahoo.com>
2021-07-23 12:49:44 -0400

    removed seed data

--- src/main/java/com/galvanize/invoicify/configuration/SeedData.java
+++ src/main/java/com/galvanize/invoicify/configuration/SeedData.java
@@ -1,20 +1,20 @@
-package com.galvanize.invoicify.configuration;
-
-import com.galvanize.invoicify.models.*;
-import com.galvanize.invoicify.repositories.BillingRecordRepository;
-import com.galvanize.invoicify.repositories.CompanyRepository;
-import com.galvanize.invoicify.repositories.InvoiceRepository;
-import com.galvanize.invoicify.repositories.UserRepository;
-import org.apache.catalina.Store;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.sql.Date;
-
-@Configuration
-public class SeedData {
-
-	public SeedData(UserRepository userRepository, CompanyRepository companyRepository, BillingRecordRepository recordRepository, InvoiceRepository invoiceRepository, PasswordEncoder encoder) {
+//package com.galvanize.invoicify.configuration;
+//
+//import com.galvanize.invoicify.models.*;
+//import com.galvanize.invoicify.repositories.BillingRecordRepository;
+//import com.galvanize.invoicify.repositories.CompanyRepository;
+//import com.galvanize.invoicify.repositories.InvoiceRepository;
+//import com.galvanize.invoicify.repositories.UserRepository;
+//import org.apache.catalina.Store;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//import java.sql.Date;
+//
+//@Configuration
+//public class SeedData {
+//
+//	public SeedData(UserRepository userRepository, CompanyRepository companyRepository, BillingRecordRepository recordRepository, InvoiceRepository invoiceRepository, PasswordEncoder encoder) {
 		User admin = userRepository.save(new User("admin", encoder.encode("admin")));
 //		User userBob = userRepository.save(new User("bob", encoder.encode("password")));
 //		User userBobby = userRepository.save(new User("bobby", encoder.encode("password")));
@@ -24,6 +24,6 @@ public class SeedData {
 //		FlatFeeBillingRecord record1 = recordRepository.save(new FlatFeeBillingRecord(admin, "Faxes", company1, 300));
 //		Date d1 = new Date (2021-07-21);
 //	    Invoice inv1 = invoiceRepository.save(new Invoice(company1, d1, admin, "Invoice test", null ));
-	}
-
-}
+//	}
+//
+//}
