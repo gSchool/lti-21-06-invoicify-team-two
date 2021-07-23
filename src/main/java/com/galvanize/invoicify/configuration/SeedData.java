58caacf9f06412486930654e1519ff746b2742d3
Lisa Lampen <monalisa408@yahoo.com>
2021-07-23 14:14:05 -0400

    comment out seed data

--- src/main/java/com/galvanize/invoicify/configuration/SeedData.java
+++ src/main/java/com/galvanize/invoicify/configuration/SeedData.java
@@ -1,29 +1,29 @@
-package com.galvanize.invoicify.configuration;
-
-import com.galvanize.invoicify.models.*;
-import com.galvanize.invoicify.repositories.BillingRecordRepository;
-import com.galvanize.invoicify.repositories.CompanyRepository;
-import com.galvanize.invoicify.repositories.InvoiceRepository;
-import com.galvanize.invoicify.repositories.UserRepository;
-import org.apache.catalina.Store;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.sql.Date;
-
-@Configuration
-public class SeedData {
-
-	public SeedData(UserRepository userRepository, CompanyRepository companyRepository, BillingRecordRepository recordRepository, InvoiceRepository invoiceRepository, PasswordEncoder encoder) {
-		User admin = userRepository.save(new User("admin", encoder.encode("admin")));
-		User userBob = userRepository.save(new User("bob", encoder.encode("password")));
-		User userBobby = userRepository.save(new User("bobby", encoder.encode("password")));
-		User userSally = userRepository.save(new User("sally", encoder.encode("password")));
-		User userCindy = userRepository.save(new User("cindy", encoder.encode("password")));
-		Company company1 = companyRepository.save(new Company("ABC Company"));
-		FlatFeeBillingRecord record1 = recordRepository.save(new FlatFeeBillingRecord(admin, "Faxes", company1, 300));
-		Date d1 = new Date (2021-07-21);
-	    Invoice inv1 = invoiceRepository.save(new Invoice(company1, d1, admin, "Invoice test", null ));
-	}
-
-}
+//package com.galvanize.invoicify.configuration;
+//
+//import com.galvanize.invoicify.models.*;
+//import com.galvanize.invoicify.repositories.BillingRecordRepository;
+//import com.galvanize.invoicify.repositories.CompanyRepository;
+//import com.galvanize.invoicify.repositories.InvoiceRepository;
+//import com.galvanize.invoicify.repositories.UserRepository;
+//import org.apache.catalina.Store;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//import java.sql.Date;
+//
+//@Configuration
+//public class SeedData {
+//
+//	public SeedData(UserRepository userRepository, CompanyRepository companyRepository, BillingRecordRepository recordRepository, InvoiceRepository invoiceRepository, PasswordEncoder encoder) {
+//		User admin = userRepository.save(new User("admin", encoder.encode("admin")));
+//		User userBob = userRepository.save(new User("bob", encoder.encode("password")));
+//		User userBobby = userRepository.save(new User("bobby", encoder.encode("password")));
+//		User userSally = userRepository.save(new User("sally", encoder.encode("password")));
+//		User userCindy = userRepository.save(new User("cindy", encoder.encode("password")));
+//		Company company1 = companyRepository.save(new Company("ABC Company"));
+//		FlatFeeBillingRecord record1 = recordRepository.save(new FlatFeeBillingRecord(admin, "Faxes", company1, 300));
+//		Date d1 = new Date (2021-07-21);
+//	    Invoice inv1 = invoiceRepository.save(new Invoice(company1, d1, admin, "Invoice test", null ));
+//	}
+//
+//}
