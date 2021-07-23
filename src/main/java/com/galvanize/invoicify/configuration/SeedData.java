d3ce1488ec27e79cabc180c71675289d78b2f2b1
jeanmarcjulien <julienjeanmarc@gmail.com>
2021-07-23 09:09:35 -0500

    Update SeedData.java

--- src/main/java/com/galvanize/invoicify/configuration/SeedData.java
+++ src/main/java/com/galvanize/invoicify/configuration/SeedData.java
@@ -1,21 +1,21 @@
-package com.galvanize.invoicify.configuration;
+// package com.galvanize.invoicify.configuration;
 
-import com.galvanize.invoicify.models.*;
-import com.galvanize.invoicify.repositories.BillingRecordRepository;
-import com.galvanize.invoicify.repositories.CompanyRepository;
-import com.galvanize.invoicify.repositories.InvoiceRepository;
-import com.galvanize.invoicify.repositories.UserRepository;
-import org.apache.catalina.Store;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.password.PasswordEncoder;
+// import com.galvanize.invoicify.models.*;
+// import com.galvanize.invoicify.repositories.BillingRecordRepository;
+// import com.galvanize.invoicify.repositories.CompanyRepository;
+// import com.galvanize.invoicify.repositories.InvoiceRepository;
+// import com.galvanize.invoicify.repositories.UserRepository;
+// import org.apache.catalina.Store;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.sql.Date;
+// import java.sql.Date;
 
-@Configuration
-public class SeedData {
+// @Configuration
+// public class SeedData {
 
-	public SeedData(UserRepository userRepository, CompanyRepository companyRepository, BillingRecordRepository recordRepository, InvoiceRepository invoiceRepository, PasswordEncoder encoder) {
-		User admin = userRepository.save(new User("admin", encoder.encode("admin")));
-	}
+// 	public SeedData(UserRepository userRepository, CompanyRepository companyRepository, BillingRecordRepository recordRepository, InvoiceRepository invoiceRepository, PasswordEncoder encoder) {
+// 		User admin = userRepository.save(new User("admin", encoder.encode("admin")));
+// 	}
 
-}
+// }
