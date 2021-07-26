a48a432a9214de2dffff3e607dc9c875990bcb15
Elias Binchamo <eliasbinchamo@gmail.com>
2021-07-26 17:24:44 -0400

    origin changed

--- src/main/java/com/galvanize/invoicify/configuration/SecurityConfig.java
+++ src/main/java/com/galvanize/invoicify/configuration/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type", "Access-Control-Allow-Origin"));
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("https://front-end-v1.herokuapp.com"));
 
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PUT","OPTIONS","PATCH", "DELETE"));
         corsConfiguration.setAllowCredentials(true);
